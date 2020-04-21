b25960d41dfce7a041c2b21d47cae45aec04bc3a
aabdelrahman <ahmed.abdelrahman@mytoys.de>
2020-04-22 00:04:20 +0200

    added usefetch hook

--- src/screens/Home/sections/News.tsx
+++ src/screens/Home/sections/News.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
+import useFetch from 'use-http';
+
 import Card from '../../../components/Card';
 
 const News = () => {
+  const { loading, error, data } = useFetch(
+    'http://newsapi.org/v2/everything?apiKey=68ddf899b505404e960daf512baabe76&q=كورونا مصر',
+    []
+  ); // onMount (GET by default)
+
   return (
     <Card>
       <Text style={styles.title}>News</Text>
     </Card>
   );
-}
+};
 
 const styles = StyleSheet.create({
   title: {
-    fontSize: 15
+    fontSize: 15,
   },
 });
 
