7edf1d967e050cc0e53b5fc869a0d9698443c7e6
omarkhaled11 <omarkhaled320@hotmail.com>
2020-04-21 23:48:47 +0200

    Add marginbottom to Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -21,6 +21,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
     paddingHorizontal: 20,
     marginHorizontal: 20,
+    marginBottom: 20,
     width: width - 40,
     shadowColor: colors.grey,
     shadowOffset: {
