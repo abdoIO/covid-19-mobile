da1092ae6cf11b004d5685c910ad624716f73c59
aabdelrahman <ahmed.abdelrahman@mytoys.de>
2020-04-22 00:46:45 +0200

    added score template

--- src/screens/Home/sections/SafetyScore.tsx
+++ src/screens/Home/sections/SafetyScore.tsx
@@ -6,13 +6,16 @@ const SafetyScore = () => {
   return (
     <Card>
       <Text style={styles.title}>Safety Score</Text>
+      <Text style={styles.title}>Can I go out?</Text>
+      <Text style={styles.title}>30</Text>
+      <Text style={styles.title}>not recommended</Text>
     </Card>
   );
-}
+};
 
 const styles = StyleSheet.create({
   title: {
-    fontSize: 15
+    fontSize: 15,
   },
 });
 
