b0c40dd08e12bc150c490ec045b9883e62507025
omarkhaled11 <omarkhaled320@hotmail.com>
2020-04-21 10:44:47 +0200

    Update HomeScreen UI

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,28 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
 import BigButton from '../components/BigButton';
+import Card from '../components/Card';
 
 const HomeScreen = ({ navigation }) => {
   const navigateToBotScreen = () => navigation.navigate('Bot');
 
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <BigButton
-        title="Start Self Diagnosis"
-        onPress={navigateToBotScreen}
-      />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.header}>
+            <View>
+              <Text style={{ fontSize: 35, fontWeight: 'bold' }}>Dashboard</Text>
+            </View>
+            <View />
+          </View>
+          <Card>
+            <Text style={{ fontSize: 15 }}>Safety Score</Text>
+          </Card>
+        </ScrollView>
+        <View style={styles.fixedButtonContainer}>
+          <BigButton
+            title="Start Self Diagnosis"
+            onPress={navigateToBotScreen}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 20,
+  },
+  header: {
+    marginVertical: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignSelf: 'stretch',
+  },
+  fixedButtonContainer: {
+    position: 'absolute',
+    bottom: 40,
+  }
 });
-  
+
 export default HomeScreen;
