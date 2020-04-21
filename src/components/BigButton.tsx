67c29aed6d1497c84a604ab3d36f5d998d6f1545
omarkhaled11 <omarkhaled320@hotmail.com>
2020-04-21 10:03:03 +0200

    Add BigButton component

--- src/components/BigButton.tsx
+++ src/components/BigButton.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, Dimensions, ViewStyle } from 'react-native';
+import { colors } from '../theme';
 
-export default ({ onPress, title }) => (
-  <Button
-    title={title}
-    onPress={onPress}
-  />
+const { width } = Dimensions.get('window');
+
+type BigButtonProps = {
+  onPress: () => void;
+  title: string;
+  containerStyle?: ViewStyle;
+}
+
+export default ({ onPress, title, containerStyle } : BigButtonProps) => (
+  <TouchableOpacity onPress={onPress} style={[styles.container, containerStyle]} >
+    <Text style={styles.text}>{title}</Text>
+  </TouchableOpacity>
 )
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.blue400,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 60,
+    width: width / 1.5,
+  },
+  text: {
+    color: colors.white,
+    fontSize: 20,
+  }
+});
