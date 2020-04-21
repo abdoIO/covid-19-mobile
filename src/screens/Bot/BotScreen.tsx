893572a6dadb5f340dd6227915b0085784598593
omarkhaled11 <omarkhaled320@hotmail.com>
2020-04-21 15:06:59 +0200

    Add chatbot and its dependencies

--- src/screens/Bot/BotScreen.tsx
+++ src/screens/Bot/BotScreen.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import ChatBot from 'react-native-chatbot';
+import { StyleSheet, SafeAreaView, View } from 'react-native';
 
 const BotScreen = () => {
+  const steps = [
+    {
+      id: '1',
+      message: 'What is your name?',
+      trigger: '2',
+    },
+    {
+      id: '2',
+      user: true,
+      trigger: '3',
+    },
+    {
+      id: '3',
+      message: 'Zeby 3alek ya {previousValue}',
+      trigger: '4',
+    },
+    {
+      id: '4',
+      message: 'ha2aw aw aw aw aw',
+      end: true,
+    },
+  ]
   return (
-    <View style={styles.container}>
-      <Text>Bot Screen</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <ChatBot steps={steps} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -17,5 +46,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-  
+
 export default BotScreen;
