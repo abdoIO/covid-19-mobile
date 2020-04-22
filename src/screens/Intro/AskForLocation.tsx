a258536d1525c835e5caeab56d541c4c166abdcb
aabdelrahman <ahmed.abdelrahman@mytoys.de>
2020-04-22 17:29:46 +0200

    added extra screens

---
+++ src/screens/Intro/AskForLocation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const AskForLocation = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default AskForLocation;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/Intro/DataPrivacy.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const DataPrivacy = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default DataPrivacy;
+
+const styles = StyleSheet.create({});
