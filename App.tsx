e1eb4d893a1b834b98832f66e46a2317f668f4d1
aabdelrahman <ahmed.abdelrahman@mytoys.de>
2020-04-22 23:21:14 +0200

    fixed styling

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native'
+import { View, TouchableOpacity } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Feather } from '@expo/vector-icons';
@@ -18,24 +18,39 @@ const HomeScreenOptions = {
 const BotScreenOptions = {
   title: '',
   headerLeft: () => <BackButton />,
-}
+};
 
 const BackButton = () => {
   const navigation = useNavigation();
   return (
-    <TouchableOpacity onPress={navigation.goBack} style={{ paddingHorizontal: 10 }}>
+    <TouchableOpacity
+      onPress={navigation.goBack}
+      style={{ paddingHorizontal: 10 }}>
       <Feather name="arrow-left" size={34} color={colors.blue400} />
     </TouchableOpacity>
-  )
-}
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="HowTo" component={HowTo} options={HomeScreenOptions} />
-        <Stack.Screen name="Home" component={HomeScreen} options={HomeScreenOptions} />
-        <Stack.Screen name="Bot" component={BotScreen} options={BotScreenOptions} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={HomeScreenOptions}
+        />
+        <Stack.Screen
+          name="HowTo"
+          component={HowTo}
+          options={HomeScreenOptions}
+        />
+
+        <Stack.Screen
+          name="Bot"
+          component={BotScreen}
+          options={BotScreenOptions}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
